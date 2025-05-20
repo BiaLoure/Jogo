638ad9cbf7074a28e896a7f14f5e34feb45f5135
Bianca Emily <97242660+BiaLoure@users.noreply.github.com>
2025-05-20 11:01:24 -0300

    feat: arvores

--- Jogo/scenes/character/player/chopping_state.gd
+++ Jogo/scenes/character/player/chopping_state.gd
@@ -1,7 +1,12 @@
 extends NodeState
 
 @export var player: Player
-@export var animation_player: AnimatedSprite2D 
+@export var animation_player: AnimatedSprite2D
+@export var hit_component_collision_shape: CollisionShape2D
+
+func _ready() -> void:
+	hit_component_collision_shape.disabled = true
+	hit_component_collision_shape.position = Vector2(0, 0);
 
 func _on_process(_delta : float) -> void:
 	pass
@@ -19,16 +24,22 @@ func _on_next_transitions() -> void:
 func _on_enter() -> void:
 	if player.player_direction == Vector2.UP:
 		animation_player.play("chopping_up")
+		hit_component_collision_shape.position = Vector2(0,-18)
 	elif player.player_direction == Vector2.DOWN:
 		animation_player.play("chopping_down")
+		hit_component_collision_shape.position = Vector2(0,3)
 	elif player.player_direction == Vector2.LEFT:
 		animation_player.play("chopping_left")
+		hit_component_collision_shape.position = Vector2(-9,0)
 	elif player.player_direction == Vector2.RIGHT:
 		animation_player.play("chopping_right")
+		hit_component_collision_shape.position = Vector2(9,0)
 	else:
 		animation_player.play("chopping_down")
-		
+	
+	hit_component_collision_shape.disabled = false
 
 
 func _on_exit() -> void:
 	animation_player.stop()
+	hit_component_collision_shape.disabled = true
